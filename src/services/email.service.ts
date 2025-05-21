fdc37558cf752bbaf1620b926a0e269d8eb9a488
Vincenzo Attanasio <vinat2000@gmail.com>
2025-05-21 17:36:52 +0200

    gestione nuova mail report errore

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -12,6 +12,16 @@ export class EmailService {
   constructor(private configService: ConfigService) {}
 
   async send(data: Product[]) {
+    // Configura Nodemailer
+    const transporter = nodemailer.createTransport({
+      host: "smtps.aruba.it",
+      port: 465,
+      auth: {
+        user: this.configService.get<string>("MAIL_FROM"),
+        pass: this.configService.get<string>("PROVIDER_PW"),
+      },
+    });
+
     try {
       const transformedData = data.map((item) => {
         return {
@@ -43,16 +53,6 @@ export class EmailService {
         delimiter: ";",
       });
 
-      // Configura Nodemailer
-      const transporter = nodemailer.createTransport({
-        host: "smtps.aruba.it",
-        port: 465,
-        auth: {
-          user: this.configService.get<string>("MAIL_FROM"),
-          pass: this.configService.get<string>("PROVIDER_PW"),
-        },
-      });
-
       // Configura l'email
       const mailOptions = {
         from: this.configService.get<string>("MAIL_FROM"),
@@ -74,6 +74,18 @@ export class EmailService {
         }
       });
     } catch (error) {
+      // Configura l'email
+      const mailOptions = {
+        from: this.configService.get<string>("MAIL_FROM"),
+        to: this.configService.get<string>("MAIL_TO_ERROR"),
+        subject: `Errore articoli rilevati da ${data[0].userCode} il ${data[0].date.split(",")[0]}`,
+        text: "Il servizio di scan ha prodotto un errore",
+      };
+      await transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          throw new Error("Failed to send email, " + error);
+        }
+      });
       throw error; // Rilancia l'errore per gestirlo in altri livelli
     }
   }
